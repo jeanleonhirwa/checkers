ec995e8b00ffc76f4a51ab41bb9ed6e117a660c9
jeanleonhirwa <jeanleonhirwa@gmail.com>
2026-01-20 13:51:49 +0200

    Ninenth commit

--- checkers-app/src/components/index.ts
+++ checkers-app/src/components/index.ts
@@ -1,6 +1,3 @@
 export { Board } from './Board';
 export { Square } from './Square';
 export { Piece } from './Piece';
-export { TurnIndicator } from './TurnIndicator';
-export { Controls } from './Controls';
-export { GameInfo } from './GameInfo';
